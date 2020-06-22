52e616ea3d1eb7d69c3a0a09d2ac43d8845f87b1
Marcos Sousa Kimura <kimura.marcos@gmail.com>
2020-06-22 14:34:12 -0300

    Correcao PilarSeeder

--- database/seeds/PilarSeeder.php
+++ database/seeds/PilarSeeder.php
@@ -11,10 +11,11 @@ class PilarSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('pilares')->insert(['nome' => 'Comunicação', 'areas' => 'Comunicação']);
-        DB::table('pilares')->insert(['nome' => 'Doutrinário', 'areas' => 'Doutrinário']);
-        DB::table('pilares')->insert(['nome' => 'Formação Pedagógica', 'areas' => 'Formação Pedagógica']);
-        DB::table('pilares')->insert(['nome' => 'Saúde', 'areas' => 'Saúde']);
-        DB::table('pilares')->insert(['nome' => 'Valorização do Ser', 'areas' => 'Valorização do Ser']);
+      DB::table('pilares')->insert(['nome' => 'Comunicação', 'areas' => 'Comunicação']);
+      DB::table('pilares')->insert(['nome' => 'Doutrinário', 'areas' => 'Doutrinário']);
+      DB::table('pilares')->insert(['nome' => 'Formação Pedagógica', 'areas' => 'Formação Pedagógica']);
+      DB::table('pilares')->insert(['nome' => 'Saúde', 'areas' => 'Saúde']);
+      DB::table('pilares')->insert(['nome' => 'Valorização do Ser', 'areas' => 'Valorização do Ser']);
+
     }
 }
