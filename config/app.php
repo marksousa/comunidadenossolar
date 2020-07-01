a5ac93553327b5ffd2214bbf062774bcefbed1bf
Mark Sousa <kimura.marcos@gmail.com>
2020-07-01 14:34:48 -0300

    Deploy da aplicaçao

--- config/app.php
+++ config/app.php
@@ -118,7 +118,7 @@ return [
     | to a random, 32 character string, otherwise these encrypted strings
     | will not be safe. Please do this before deploying an application!
     |
-    */
+    */ 
 
     'key' => env('APP_KEY', 'base64:Z10KXMAmU2CuE0SYy9UI6RaVTNrZ2724HTdxvGx7CUI='),
 
