fe9557fb25fc3847a494538ece238bd4c6a54d7f
fabiomassanori <fabiomassanori@gmail.com>
2020-07-10 14:47:47 -0300

    Arrumado o PilarSeeder

--- database/seeds/PilarSeeder.php
+++ database/seeds/PilarSeeder.php
@@ -12,7 +12,7 @@ class PilarSeeder extends Seeder
     public function run()
     {
         DB::table('pilares')->insert(['nome' => 'Comunicação', 'areas' => 'Jornalismo, Mídias, Redes Sociais, Site']);
-        DB::table('pilares')->insert(['nome' => 'Doutrinário', 'areas' => 'Atendimento Fraterno, Biblioteca, Estudos, Evanelho no Lar, Evangelização infantil, Eventos, Mediúnica, Mocidade, Palestras, Recepção, Trabalhos de Caridade, Tratamento Espiritual']);
+        DB::table('pilares')->insert(['nome' => 'Doutrinário', 'areas' => 'Atendimento Fraterno, Biblioteca, Estudos, Evangelho no Lar, Evangelização infantil, Eventos, Mediúnica, Mocidade, Palestras, Recepção, Trabalhos de Caridade, Tratamento Espiritual']);
         DB::table('pilares')->insert(['nome' => 'Educação', 'areas' => 'Artes, Assistência Escolar, Educação Física / Esporte, Escola Integral, Formaçao Profissional, Informática, Línguas Estrangeiras, Música']);
         DB::table('pilares')->insert(['nome' => 'Saúde', 'areas' => 'Acupuntura, Farmácia, Fisioterapia, Fonoaudiologia, Odontologia, Medicina (Atendimento Clínico e Exames), Meditação, Nutrição, Odontologia, Psicologia, Psiquiatria, Terapia Ocupacional, Veterinária, Yoga']);
         DB::table('pilares')->insert(['nome' => 'Valorização do Ser', 'areas' => 'Abrigo, Acolha-me, Casa de Passagem, Cozinha, Doações, Gestantes, Oficinas, Jurídico, Lavanderia, Serviços']);
